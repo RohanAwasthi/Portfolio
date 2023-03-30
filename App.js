cf1f0503e81f9846617d7bff6ffc57a7aa115e1b
RohanAwasthi <128706839+RohanAwasthi@users.noreply.github.com>
2023-03-30 11:46:57 +0530

    commit1

--- App.js
+++ App.js
@@ -1,27 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import Header from './Header';
+import Footer from './footer'; 
+import { useState } from "react"
+import Tasks from './Tasks';
+
 
 function App() {
-  return (
-    <div className="App">
-      
-        <div class="H1"><h1>Welcome to react</h1></div>
-        <p className="p1">
-        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+  
 
-“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
-The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
+const [tasks, setTasks] = useState([
 
-The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.
-        </p>
-         <footer className="f1">
-          Build by Rohan Awasthi
-         </footer>
-        </div>
-         
-    
-    
-  );
+  {
+      id:1,
+      text: 'Yoga ',
+      day: '30 March 2023 at 6:00 AM',
+      reminder: true
+  },
+  {
+      id:2,
+      text: 'Meditation',
+      day: '30 March 2023 at 7:30 AM',
+      reminder: true
+  },
+  {
+      id:3,
+      text: 'Breakfast',
+      day: '30 March 2023 at 8:30 AM',
+      reminder: false
+  },
+  {
+      id:4,
+      text: 'Training',
+      day: '30 March 2023 at 9:00 AM',
+      reminder: true
+  }
+
+])
+
+
+
+const deleteTask = (id) => {
+  
+    setTasks(tasks.filter((task) => task.id !== id))
 }
+  
+  
+
+  return (
+    <div className="container">
+      <Header/>
+       { tasks.length>0 ?
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
+       :'No Task Available'}
+  </div>
+  );
+  }
 
 export default App;
